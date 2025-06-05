c3c09aa3442b4b821959ea0739275c24f0b9b241
Tuscy <gingertuscs@gmail.com>
2025-06-05 21:19:16 +0100

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Rhyse Summers on 05/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarTests/CalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarTests.swift
+//  CalendarTests
+//
+//  Created by Rhyse Summers on 05/06/2025.
+//
+
+import Testing
+@testable import Calendar
+
+struct CalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
